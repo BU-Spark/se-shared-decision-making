fe9887690cc436a6c8212220f592475d188bc22e
juniajanvier <74433856+juniajanvier@users.noreply.github.com>
2022-12-07 03:56:03 -0500

    Layout page is fixed, added props type to fix bug

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,16 +1,15 @@
 import NavBar from "./Navbar/NavBar";
 import FooterBar from "./Footer/FooterBar";
-import React, { ReactNode } from "react";
 
-// const Layout = ({ children }) => {
-//   return (
-//     <div>
-//       <NavBar />
-//       {children}
-//       <FooterBar />
-//     </div>
-//   );
-// };
-// export default Layout;
+const Layout = (props: any) => {
+  return (
+    <div>
+      <NavBar />
+      {props.children}
+      <FooterBar />
+    </div>
+  );
+};
+export default Layout;
 
-// I have the layout page up but, I have a bug with the children, will fix
+//Layout fixed and pushed to branch
