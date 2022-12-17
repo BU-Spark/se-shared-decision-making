cf0dfffc52fdc21efe080cc4c88fce2b5f42aaeb
XichengSpencer <74568012+XichengSpencer@users.noreply.github.com>
2022-12-17 13:11:00 -0500

    recover the change in nav bar

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,25 +1,13 @@
 
 import { Grid } from "@mui/material";
-import React from "react";
-
-import NavBar from "./Navbar/NavBar";
-
-import NavBar from "./Navbar/NavBar";
-
 import FooterBar from "./Footer/FooterBar";
 import Header from "./Navbar/Header";
 
 const Layout = ({ children }: any) => {
   return (
-
     <Grid container columns={{ xl: 12, lg: 12, md: 12 }}>
       <Header />
       {children}
-
-    {/* <div style={{ margin: "0", width: "100%" }}> */}
-      <NavBar />
-      {children}
-
       <FooterBar />
     </Grid>
   );
