b6280d8d0d21da8f13fa0cd3c9179f6000b2ea4e
Abhishek <Abhishek@Shaq-Malarkey.local>
2023-03-24 00:59:23 -0400

    changes for strapi

--- se-decisions-backend/config/env/development/database.js
+++ se-decisions-backend/config/env/development/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('PGHOST', '127.0.0.1'),
-      port: env.int('PGPORT', 5931),
+      host: env('PGHOST', 'containers-us-west-138.railway.app'),
+      port: env.int('PGPORT', 5865),
       database: env('PGDATABASE', 'railway'),
       user: env('PGUSER', 'postgres'),
-      password: env('PGPASSWORD', 'password'),
+      password: env('PGPASSWORD', '9tcJBdK6HqEQCdibzGuF'),
       ssl: env.bool(true),
     },
   },
