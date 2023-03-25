2afac41daf44e45185ef9dc71a0397704c58e397
Abhishek <Abhishek@Shaq-Malarkey.local>
2023-03-24 21:54:30 -0400

    db url

--- se-decisions-backend/config/env/production/database.js
+++ se-decisions-backend/config/env/production/database.js
@@ -2,6 +2,7 @@ module.exports = ({ env }) => ({
     connection: {
       client: 'postgres',
       connection: {
+        connectionString: env('postgresql://postgres:bakdIdhaWdfU5jzQRpAl@containers-us-west-190.railway.app:7368/railway'),
         host: env('PGHOST', 'containers-us-west-190.railway.app'),
         port: env.int('PGPORT', 7368),
         database: env('PGDATABASE', 'railway'),
