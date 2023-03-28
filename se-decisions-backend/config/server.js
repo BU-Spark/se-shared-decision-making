62fc5289a071bef29a417ee700d7067366f3d265
Abhishek <Abhishek@crc-dot1x-nat-10-239-115-16.bu.edu>
2023-03-28 18:13:17 -0400

    Server url change

--- se-decisions-backend/config/server.js
+++ se-decisions-backend/config/server.js
@@ -7,4 +7,5 @@ module.exports = ({ env }) => ({
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
+  url: "https://se-shared-decision-making-dev.up.railway.app"
 });
