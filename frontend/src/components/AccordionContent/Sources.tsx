38baa83e5b8dd5339207e9a1ab73932dedd8dfb6
Weiru He <lingqingran@gmail.com>
2023-03-28 17:41:53 -0400

    change sources component

--- frontend/src/components/AccordionContent/Sources.tsx
+++ frontend/src/components/AccordionContent/Sources.tsx
@@ -1,30 +1,52 @@
-import { CircularProgress } from '@mui/material';
-import {Grid, Typography } from "@mui/material";
+import { Grid, Typography } from '@mui/material'
 
-export const Sources=({number, text1, text2}:any)=>{
-    
-  return(
-    <Grid  container >
-      <Grid item borderRadius="50%" bgcolor="#DFF0D8" sx={{width:"50px",height:"50px"}}
-      textAlign="center" alignItems="center" display="flex" flexDirection="column" justifyContent="center">
-        <Typography variant='h6' fontFamily="Clash Grotesk" fontWeight="500" fontSize="24px"
-        lineHeight="26px" >{number}</Typography>
-      </Grid>
-      <Grid item  sx={{ml:"32px",width:"720px"}}>
-        <Typography paragraph fontFamily="Public Sans" fontWeight="400"
-         fontSize="14px" lineHeight="20px"  display="inline"
-         color="#000000">
-           {text1}
-        </Typography>
-        <Typography display="inline"
-        fontFamily="Public Sans"
-        fontWeight="700"
-        fontSize="16px" lineHeight="24px" 
-        sx={{ml:"10px"}}
+export const Sources = ({ number, text1, text2 }: any) => {
+  return (
+    <Grid container>
+      <Grid
+        item
+        borderRadius="50%"
+        bgcolor="#DFF0D8"
+        sx={{ width: '50px', height: '50px' }}
+        textAlign="center"
+        alignItems="center"
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+      >
+        <Typography
+          variant="h6"
+          fontFamily="Clash Grotesk"
+          fontWeight="500"
+          fontSize="24px"
+          lineHeight="26px"
         >
-            {text2}
+          {number}
+        </Typography>
+      </Grid>
+      <Grid item sx={{ ml: '32px', width: '720px' }}>
+        <Typography
+          paragraph
+          fontFamily="Public Sans"
+          fontWeight="400"
+          fontSize="14px"
+          lineHeight="20px"
+          display="inline"
+          color="#000000"
+        >
+          {text1}
+        </Typography>
+        <Typography
+          display="inline"
+          fontFamily="Public Sans"
+          fontWeight="700"
+          fontSize="16px"
+          lineHeight="24px"
+          sx={{ ml: '10px' }}
+        >
+          {text2}
         </Typography>
       </Grid>
     </Grid>
-  );
+  )
 }
