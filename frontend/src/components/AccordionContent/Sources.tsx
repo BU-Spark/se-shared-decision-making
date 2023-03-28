a26dd8d050f04fed920b6354d47c6c2c2137bd49
dfjhlsdhf <qinchen4@outlook.com>
2023-03-28 17:38:10 -0400

    init Sources component

--- frontend/src/components/AccordionContent/Sources.tsx
+++ frontend/src/components/AccordionContent/Sources.tsx
@@ -4,13 +4,13 @@ import {Grid, Typography } from "@mui/material";
 export const Sources=({number, text1, text2}:any)=>{
     
   return(
-    <Grid  container xl={12} lg={12} md={12}>
+    <Grid  container >
       <Grid item borderRadius="50%" bgcolor="#DFF0D8" sx={{width:"50px",height:"50px"}}
       textAlign="center" alignItems="center" display="flex" flexDirection="column" justifyContent="center">
         <Typography variant='h6' fontFamily="Clash Grotesk" fontWeight="500" fontSize="24px"
         lineHeight="26px" >{number}</Typography>
       </Grid>
-      <Grid item  sx={{ml:"32px",width:"620px"}}>
+      <Grid item  sx={{ml:"32px",width:"720px"}}>
         <Typography paragraph fontFamily="Public Sans" fontWeight="400"
          fontSize="14px" lineHeight="20px"  display="inline"
          color="#000000">
