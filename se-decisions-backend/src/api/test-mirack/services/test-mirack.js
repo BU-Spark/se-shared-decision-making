495bb00d7bd1a5b64eb3f5546b9177ee03aace6e
Mirack <mirackchen@gmail.com>
2023-04-13 00:07:05 -0400

    add types for myvalues page

--- se-decisions-backend/src/api/test-mirack/services/test-mirack.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * test-mirack service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::test-mirack.test-mirack');
